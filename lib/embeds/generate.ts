2893343494381217504bde6b6ebf63421f5506a5
Dhairya Gupta <dhairyag23@gmail.com>
2024-01-12 22:36:19 -0600

    Add back to lowercase tracking

---
+++ lib/embeds/generate.ts
@@ -0,0 +1,22 @@
+import { EmbedBuilder } from "discord.js";
+import constants from "../../constants";
+
+export function getStandardEmbed(
+  command: string,
+  purpose: string
+): EmbedBuilder {
+  const embed = new EmbedBuilder();
+
+  embed.setFooter({
+    text: `${command}  •  Scoultimate v${constants.version}`,
+    iconURL: constants.bot_logo_url,
+  });
+
+  embed.setTimestamp();
+
+  embed.setAuthor({
+    name: `Scoultimate ${purpose}`,
+  });
+
+  return embed;
+}
