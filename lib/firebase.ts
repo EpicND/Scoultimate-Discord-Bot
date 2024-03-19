3447c8e2493178c97576c6b1bc22a9997b9c2b32
Dhairya Gupta <dhairyag23@gmail.com>
2024-03-19 16:12:12 -0500

    Command Counters for Statistics on the Backend

---
+++ lib/firebase.ts
@@ -0,0 +1,21 @@
+import { App, cert } from "firebase-admin/app";
+import * as admin from "firebase-admin";
+import { getFirestore, FieldValue } from "firebase-admin/firestore";
+
+const app: App = admin.initializeApp({
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
+
+export const db = getFirestore(app);
+
+export async function incrementTotalCalls(cmd: string) {
+  const ref = db.collection("bot").doc("statistics");
+  await ref.update({
+    total_cmd_used: FieldValue.increment(1),
+    [cmd]: FieldValue.increment(1),
+  });
+}
